857c8bc64c99373ac9413b42915ee5e20c25cb9a
mrluisperez <lkperezospino@gmail.com>
2023-01-03 21:26:53 -0600

    callback examples

---
+++ src/callback/index.js
@@ -0,0 +1,22 @@
+//example 1
+function suma (num1, num2) {
+    return num1 + num2;
+}
+
+function calc (num1, num2, callback) {
+    return callback(num1, num2);
+}
+
+console.log(calc(2, 2, suma));
+
+//example 2
+setTimeout(function(){
+    console.log('Hola JavaScript');
+}, 5000)
+
+//example 3
+function gretting(name){
+    console.log(`Hola ${name}`);
+}
+
+setTimeout(gretting, 2000, 'Luis');
