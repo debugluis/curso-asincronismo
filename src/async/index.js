d50caf6dd77d8c6c6f17d18db93a087adfd9ca86
mrluisperez <lkperezospino@gmail.com>
2023-01-07 20:56:22 -0600

    async funtions examples

---
+++ src/async/index.js
@@ -0,0 +1,16 @@
+const fnAsync = () => {
+    return new Promise((resolve, reject) => {
+        (true) ? setTimeout(() => resolve('Async!!'), 2000)
+        : reject(new Error('Error!!'));
+    });
+}
+
+const anotherFn = async () => {
+    const someting = await fnAsync();
+    console.log(someting);
+    console.log('hello');
+}
+
+console.log('before');
+anotherFn();
+console.log('after');
